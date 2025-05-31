b62c90de55277f2ec0a960ad74a4d39291eccf06
Jelatz <lanetejlad@gmail.com>
2025-06-01 03:37:14 +0800

    initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mt-10 bg-red-600">
+        <h1>heyasdasdasdasdasdads</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::inertia('/', 'Home')->name('home');
+// Route::Inertia('/', 'Home');
